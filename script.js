fbc4655e024a5b24d8ff933673fe06e5fd085276
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-06 20:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const nameInput = document.getElementById("name");
+const yearInput= document.getElementById("year");
+const Button = document.getElementById("button");
+const URL = document.getElementById("url");
+
+Button.addEventListener("click", function(){
+let	name = nameInput.value;
+let	year =  yearInput.value;
+
+	let newURL = 'https://localhost:8080/?name=' + name + '&year=' + year;
+
+	URL.textContent = newURL;
+	
+});
